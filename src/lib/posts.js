2fb57c989871656897e0c661b94ccf5a3c4899b0
Khdulkadir <khdulkadir@gmail.com>
2024-10-10 22:04:38 +0200

    content toegevoegd

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -1,14 +1,26 @@
 export const posts = [
-    {
-        slug: 'my-first-post',
-        title: 'My First Blog Post',
-        date: '10 Oktober 2024',
-        content: 'This is the content of my first blog post. It\'s simple text without markdown.'
-    },
-    {
-        slug: 'another-post',
-        title: 'Another Blog Post',
-        date: '12 Oktober 2024',
-        content: 'Here is some more content for another blog post. You can add more as needed!'
-    }
+  {
+    slug: 'we-love-web-gemert',
+    title: 'We Love Web - Gemert',
+    date: '13 September 2024',
+    content: 'Here is some more content for another blog post. You can add more as needed!'
+  },
+  {
+    slug: 'sprint-13',
+    title: 'Sprint 13',
+    date: '20 September 2024',
+    content: 'This is the content of my first blog post. It\'s simple text without markdown.'
+	},
+	{
+		slug: 'we-love-web-garnier',
+		title: 'We Love Web - Garnier',
+		date: '4 Oktober 2024',
+    content: 'This is the content of my first blog post. It\'s simple text without markdown.'
+  },
+  {
+    slug: 'sprint-14',
+    title: 'Sprint 14',
+    date: '11 Oktober 2024',
+		content: 'This is the content of my first blog post. It\'s simple text without markdown.'
+  },
 ];
